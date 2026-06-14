c20c50c3267c593721badd54c8f498a50a3bf650
nithin-arthub <nithinsjps@gmail.com>
2026-06-14 17:39:23 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> answer = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++)
+        {
+            Set<Integer> dummy = new HashSet<>();
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(dummy.contains(-(nums[i]+nums[j])))
+                {
+                    answer.add(Arrays.asList(nums[i],nums[j],(-(nums[i]+nums[j]))));
+                }
+                dummy.add(nums[j]);
+            }
+        }
+        return new ArrayList<>(answer);
+    }
+}

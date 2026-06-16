c0e46ea1143ec2aa2a599fa66c71deaa33730135
nithin-arthub <nithinsjps@gmail.com>
2026-06-16 11:24:53 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        //Arrays.sort(prices);
+        
+        int ans=0;
+        for(int i=0;i<prices.length;i++)
+        {
+           int entry=prices[i];
+            for(int j=i;j<prices.length;j++)
+            {
+               int exit=prices[j];
+            
+                if(exit>entry)
+                {
+                    int currentprice=exit-entry;
+                    ans=Math.max(currentprice,ans);
+                }
+            }
+            
+        }
+        return ans ;
+    }
+}

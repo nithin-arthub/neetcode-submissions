fa24b29d1c1652b476de7b93229501e00ac804f7
nithin-arthub <nithinsjps@gmail.com>
2026-07-04 12:44:34 +0530

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       int left=0;
+       int right=s.length()-1;
+       while(left<right)
+       {
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left)))
+            {
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right)))
+            {
+                right--;
+            }
+            char left_ch=Character.toLowerCase(s.charAt(left));
+            char right_ch=Character.toLowerCase(s.charAt(right));
+
+            if(left_ch!=right_ch)
+            {
+                return false;
+            }
+            left++;
+            right--;     
+       }
+       return true;
+    }
+}
